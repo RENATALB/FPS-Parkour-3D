18def7e89a31027cd3dc5c7ae0d69e0f6824da73
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-06-04 15:17:11 -0300

    FindObjectOfType

--- Assets/MercanciaScript.cs
+++ Assets/MercanciaScript.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class MercanciaScript : MonoBehaviour
 {
     public int precio;
+    public MoneyManager moneyManager;
 
+    private void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
         if(collision.gameObject.name == "Player")
         {
+            moneyManager.UpdateMoney(-precio);
             Destroy(gameObject);
         }
     }
---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+
+    public void UpdateMoney(float amount)
+    {
+        if (money + amount < 0)
+        {
+            Debug.Log("No te alcanza");
+        }
+        else
+        {
+            money += amount;
+        }
+    }
+}
